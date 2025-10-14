edd1385c3f334d612a7babfb3ce447e4e328c8a8
Umair Hasan <ioopl.com@gmail.com>
2025-10-14 19:38:15 +0100

    Initial Commit

---
+++ Aviir/AviirApp.swift
@@ -0,0 +1,17 @@
+//
+//  AviirApp.swift
+//  Aviir
+//
+//  Created by Umair Hasan on 14/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AviirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
