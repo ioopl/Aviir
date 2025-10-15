bf07d62cd0ad0bbab67254f0aa470de788beaef2
Umair Hasan <ioopl.com@gmail.com>
2025-10-15 14:05:32 +0100

    chore: removed commented out code

--- Aviir/View/Root/RootTabView.swift
+++ Aviir/View/Root/RootTabView.swift
@@ -11,28 +11,16 @@ struct RootTabView: View {
             MainView(viewModel: mainViewModel)
                 .tabItem {
                     Image(systemName: "house.fill")
-                    //Text("Main")
                 }.tag(AppCoordinator.Tab.home)
             
             BLEView(vm: BLEViewModel(service: coordinator.bleService))
                 .tabItem {
                     Image(systemName: "dot.radiowaves.left.and.right")
-                    //Text("BLE")
                 }
                 .tag(AppCoordinator.Tab.bluetooth)
-            /*
-            UWBView(vm: UWBViewModel(uwb: coordinator.uwbService, connector: coordinator.peerConnector))
-                .tabItem {
-                    Image(systemName: "wave.3.right") // or "location.north.line"
-                    Text("UWB")
-                }
-                .tag(AppCoordinator.Tab.uwb)
-            */
-            
             SettingsView()
                 .tabItem {
                     Image(systemName: "gearshape")
-                    Text("Settings")
                 }
                 .tag(AppCoordinator.Tab.settings)
         }
